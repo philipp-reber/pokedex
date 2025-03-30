35b1a1477b6826e4c223a406f4a926071a076536
philipp-reber <ph.reber@gmail.com>
2025-03-30 12:58:28 +0200

    pokemon search

--- cmd_exithelp.go
+++ cmd_exithelp.go
@@ -5,13 +5,13 @@ import (
 	"os"
 )
 
-func commandExit(cfg *config) error {
+func commandExit(cfg *config, args ...string) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp(cfg *config) error {
+func commandHelp(cfg *config, args ...string) error {
 	fmt.Println()
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
